efa50e69fe94e632d14443aed6e1af7d651ecd3b
Arunima Dutta <arunimadutta16@gmail.com>
2023-10-19 19:30:45 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
-console.log('testing')
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+let apiQuotes = [];
+function newQuote(){
+    //a random quote
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    if(!quote.author){
+        authorText.textContent = 'unknown';
+    }
+    else{
+    
+    authorText.textContent = quote.author;
+    }
+    if(quote.text.length > 50){
+        quoteText.classList.add('long-quote');
+    }
+    else{
+        quoteText.classList.remove('long-quote');
+    }
+    quoteText.textContent = quote.text;
+}
+//get quotes from api
+async function getQuotes(){
+    const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
+    try{
+      const response = await fetch(apiUrl);
+      apiQuotes = await response.json();
+      //console.log(apiQuotes[12]);
+      newQuote();
+    } catch(error){
+
+    }
+    //on load
+    
+}
+function tweetQuote(){
+    const twitterUrl = `
+    https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+getQuotes();
